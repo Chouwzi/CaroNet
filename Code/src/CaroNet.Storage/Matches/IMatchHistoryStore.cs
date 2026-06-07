a082d7db7c1ecface5610560d1aebb3a22ce5b28
Nguyễn Trần Đình Chương <ntd.chuong@gmail.com>
2026-06-07 12:33:20 +0700

    Revert "Merge pull request #10 from Chouwzi/feature/7-match-history-storage"
    This reverts commit bba77a8728e450bd8e94e90643b53ec0c3abd61b, reversing
    changes made to d45a893c396b001c0b64dece48eb8616779a1929.

--- Code/src/CaroNet.Storage/Matches/IMatchHistoryStore.cs
+++ Code/src/CaroNet.Storage/Matches/IMatchHistoryStore.cs
@@ -2,14 +2,4 @@ namespace CaroNet.Storage.Matches;
 
 public interface IMatchHistoryStore
 {
-    Task SaveMatchAsync(
-        MatchRecord match,
-        CancellationToken cancellationToken = default);
-
-    Task<MatchRecord?> GetMatchAsync(
-        Guid matchId,
-        CancellationToken cancellationToken = default);
-
-    Task<IReadOnlyList<MatchRecord>> GetAllMatchesAsync(
-        CancellationToken cancellationToken = default);
 }
