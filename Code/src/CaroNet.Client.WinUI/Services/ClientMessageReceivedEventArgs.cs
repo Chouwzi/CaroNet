6725a1db0b79520d633f5de11f623ffaa63ea959
phucnh8317-coder <phucnh8317@ut.edu.vn>
2026-06-13 08:35:17 +0700

    feat: implement chat service

---
+++ Code/src/CaroNet.Client.WinUI/Services/ClientMessageReceivedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CaroNet.Shared.Protocol;
+
+namespace CaroNet.Client.WinUI.Services
+{
+    public sealed class ClientMessageReceivedEventArgs : EventArgs
+    {
+        public MessageEnvelope Message { get; }
+
+        public ClientMessageReceivedEventArgs(MessageEnvelope message)
+        {
+            Message = message;
+        }
+    }
+}
---
+++ Code/src/CaroNet.Client.WinUI/Services/IClientConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using CaroNet.Shared.Protocol;
+
+namespace CaroNet.Client.WinUI.Services
+{
+    public interface IClientConnection : IAsyncDisposable
+    {
+        bool IsConnected { get; }
+
+        event EventHandler<ClientMessageReceivedEventArgs>? MessageReceived;
+        event EventHandler<string>? Disconnected;
+
+        Task ConnectAsync(string host, int port);
+        Task SendAsync(MessageEnvelope message);
+        Task DisconnectAsync();
+    }
+}
